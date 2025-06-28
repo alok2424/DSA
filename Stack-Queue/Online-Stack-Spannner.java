e1acc6d7c256d668fe3b02ff0b8dd7b65c3f6c32
alok2424 <22ec059@jssaten.ac.in>
2025-06-28 10:47:43 +0530

    "Stack-Spanner"

--- Stack-Queue/Online-Stack-Spannner.java
+++ Stack-Queue/Online-Stack-Spannner.java
@@ -1,31 +1,21 @@
 import java.util.*;
-class StockSpanner{
-    //stack stores: prices and index in array
+class StockSpanner {
+    // Stack stores: [price, span]
     private Stack<int[]> st;
-    private int index;
 
-   //cosntructor
-    public StockSpanner(){
+    public StockSpanner() {
         st = new Stack<>();
-        index = -1;
     }
 
+    public int next(int price) {
+        int span = 1;
 
-   //Method to compute stock span
-   public int next(int price){
-    index++;
-    //store only prices greater than current index
-    //so if peek element in the stack is less than or equal to current value then pop it 
-    while(!st.isEmpty() && st.peek()[0]<=price){
-       st.pop();
+        // Pop and accumulate spans of smaller or equal prices
+        while (!st.isEmpty() && st.peek()[0] <= price) {
+            span += st.pop()[1];
+        }
+
+        st.push(new int[]{price, span});
+        return span;
     }
-    //find previousGreaterIndex
-    int previousGreaterIndex = st.isEmpty() ? -1 : st.peek()[1];
-    //find the span value
-    int span = index - previousGreaterIndex;
-
-    //push price and index into stack
-    st.push(new int[]{price,index});
-    return span;
-   }
 }
