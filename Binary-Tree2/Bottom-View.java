cd003c6b81868059b1ab4da7764ba30e84012c10
alok2424 <22ec059@jssaten.ac.in>
2025-07-07 11:17:32 +0530

    "bottom-view"

---
+++ Binary-Tree2/Bottom-View.java
@@ -0,0 +1,48 @@
+
+import java.util.*;
+class TreeNode{
+   int data;
+   TreeNode left,right;
+   TreeNode(int _data){
+      this.data = _data;
+    }
+}
+class Pair{
+    TreeNode node;
+    int hd;
+    Pair(TreeNode _node,int _hd){
+        this.node = _node;
+        this.hd= _hd;
+    }
+}
+class Solution{
+    public static ArrayList<Integer> bottomView(TreeNode root){
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(root == null){
+            return  ans;
+        }
+        Map<Integer,Integer> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+
+        q.add(new Pair(root,0));
+
+        while(!q.isEmpty()){
+            Pair it = q.poll();
+            int hd = it.hd;
+            TreeNode temp = it.node;
+
+            map.put(hd,temp.data);
+
+            if(temp.left != null){
+                q.add(new Pair(temp.left,hd-1));
+            }
+            if(temp.right != null){
+                q.add(new Pair(temp.right,hd+1));
+            }
+        }
+        for(int values: map.values()){
+            ans.add(values);
+        }
+    return ans;
+    }
+}
